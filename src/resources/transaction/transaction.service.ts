6e1e6b413dd826cbc5a2b227a65d4e07081bf19d
baxromxoja16 <aglamof@mail.ru>
2024-02-06 16:10:44 +0500

    fix: account balance changes when create and delete transaction

--- src/resources/transaction/transaction.service.ts
+++ src/resources/transaction/transaction.service.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import Account from '../account/account.model'
 
 import Transaction from './transaction.model';
 
@@ -25,6 +26,23 @@ export async function getTransById(req: Request, res: Response, next: NextFuncti
 
 export async function createTrans(req: Request, res: Response, next: NextFunction) {
   try {
+    const account = await Account.findById(req.params.accountId);
+
+    // Tranzaksiya miqdorini olish
+    const { amount, type } = req.body;
+
+    // Tranzaksiya turi "expense" bo'lsa, balansdan ayirish
+    if (type === "expense" && account !== null) {
+      account.balance -= amount;
+    } else if (type === "income" && account !== null) {
+      // Tranzaksiya turi "income" bo'lsa, balansga qo'shish
+      account.balance += amount;
+    }
+
+    // Hisobni yangilash
+    await account?.save();
+
+    // Tranzaksiyani qo'shish
     const transaction = await Transaction.create({ ...req.body, accountId: req.params.accountId });
     res.json(transaction);
   } catch (err) {
@@ -44,9 +62,27 @@ export async function updateTrans(req: Request, res: Response, next: NextFunctio
 
 export async function deleteTrans(req: Request, res: Response, next: NextFunction) {
   try {
-    const { id } = req.params;
-    await Transaction.findByIdAndDelete(id);
-    res.status(204).json();
+   // Tranzaksiyani o'chirish
+   const transaction = await Transaction.findByIdAndDelete(req.params.id);
+
+   if (!transaction) {
+     return res.status(404).json({ message: "Tranzaksiya topilmadi" });
+   }
+
+   // Hisobni olish
+   const account = await Account.findById(transaction.accountId);
+
+   // Tranzaksiya miqdorini qayta hisoblash
+   if (transaction.type === "expense" && account) {
+     account.balance += transaction.amount;
+   } else if (transaction.type === "income" && account) {
+     account.balance -= transaction.amount;
+   }
+
+   // Hisobni yangilash
+   await account?.save();
+
+   res.status(200).json();
   } catch (err) {
     next(err);
   }
